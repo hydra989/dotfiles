eb140e182db9b84f74f28a9e2437245efa16be29
hydra989 <c.carb0n@protonmail.ch>
2022-05-20 22:41:29 -0500

    upstream changes so i push nix configs from vm

--- sh/proxmox.sh
+++ sh/proxmox.sh
@@ -3,4 +3,17 @@
 # proxmox.sh
 # do some setup-type things on a fresh proxmox install
 
-# TODO
+
+# download dark theme
+wget https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.py
+python3 PVEDiscordDark.py
+
+
+# update
+apt update && apt dist-upgrade
+
+
+# remind
+echo "don't forget to setup the community subscription repository"
+echo "and remove the web gui soft paywall!"
+echo "happy hacking :)"
