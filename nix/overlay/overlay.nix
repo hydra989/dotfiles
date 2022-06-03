70207c61422628e24c0ddf4c8948337317afa830
Hayden Curfman <hcurfman@keemail.me>
2022-06-03 01:21:57 -0400

    keep messing with dwm overlay

--- nix/overlay/overlay.nix
+++ nix/overlay/overlay.nix
@@ -1,20 +1,11 @@
 {}: self: super:
 {
   dwm = super.dwm.overrideAttrs (oldAttrs: rec {
-    version = "6.2";
-    patches = [
-      (super.fetchpatch {
-        url = "https://raw.githubusercontent.com/hydra989/dotfiles/master/dwm/implement-deity-mode.patch";
-        sha256 = "129xj5zbwrrhsqp0izi0603kizk3vmg005f1368zwz2gag6f08a6";
-      })
-      (super.fetchpatch {
-        url = "https://raw.githubusercontent.com/hydra989/dotfiles/master/dwm/xresources-6.2.patch";
-        sha256 = "03pm4whzbq8vmqki8lzbhdfbfz2xfrzcgirp0vfa12x5j5iyyhm4";
-      })
-      (super.fetchpatch {
-        url = "https://raw.githubusercontent.com/hydra989/dotfiles/master/dwm/header.patch";
-        sha256 = "10m1rpmfcxijmfm7axi779c2whk5rc00sgwvhc9dig69fs8wxqjc";
-      })
-    ];
+    src = fetchFromGithub {
+      owner = "hydra989";
+      repo = "dwm-6.2-fork";
+      rev = "25a1954a81c8c389591270ef0c7bd535663bca8d";
+      sha256 = "05xjjgyfsacywgriwslhmx0ypb6nyfc3f4fpwp44chzah684l21h";
+    };
   });
 }
