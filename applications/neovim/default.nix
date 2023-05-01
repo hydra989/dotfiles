d354f44cc3557e4c20be3521208d5675167ade3b
Hayden Curfman <hcurfman@keemail.me>
2023-04-30 23:58:44 -0400

    add neovim compatibility config

--- applications/neovim/default.nix
+++ applications/neovim/default.nix
@@ -1,6 +1,11 @@
-{ ... }:
-{
-  programs.neovim = {
-    enable = true;
+{ ... }: {
+  programs.neovim = { enable = true; };
+
+  home.file."/.config/nvim/init.vim" = {
+    text = ''
+      set runtimepath^=~/.vim runtimepath+=~/.vim/after
+      let &packpath = &runtimepath
+      source ~/.vimrc
+    '';
   };
 }
